3a0e090abd7a5e567c2f02ec08241b630a38cc29
MohsenRazavi <mohsenrazavi.self@gmail.com>
2023-03-14 00:10:24 +0330

    complete memory

--- tools/memory.py
+++ tools/memory.py
@@ -2,4 +2,10 @@ class Memory:
     memory = {str(i): 11 * '0' for i in range(0, 10000)}
 
     def load(self, index):
-        return self.memory[index]
+        return self.memory[str(index)]
+
+    def store(self, index, value):
+        self.memory[str(index)] = str(value)
+
+    def clear(self):
+        self.memory = {str(i): 11 * '0' for i in range(0, 10000)}
